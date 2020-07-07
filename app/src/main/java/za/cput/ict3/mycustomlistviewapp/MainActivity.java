073ba46ca098e69d47d43000d577f369469e1b8b
solly <214293939@mycput.ac.za>
2020-07-07 15:09:51 +0200

    Testing sync.

---
+++ app/src/main/java/za/cput/ict3/mycustomlistviewapp/MainActivity.java
@@ -0,0 +1,19 @@
+package za.cput.ict3.mycustomlistviewapp;
+/**
+ * Author: Sonwabo Kasi
+ * Class: Part Time
+ * Student number: 214293939
+ * Class Description: 
+ */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
