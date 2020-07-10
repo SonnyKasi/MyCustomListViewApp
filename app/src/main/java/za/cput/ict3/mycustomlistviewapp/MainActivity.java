ea2935a16443f3d1d48f0305fa4808b5a46181ab
solly <214293939@mycput.ac.za>
2020-07-10 17:10:44 +0200

    Classes built, need to sort errors.

--- app/src/main/java/za/cput/ict3/mycustomlistviewapp/MainActivity.java
+++ app/src/main/java/za/cput/ict3/mycustomlistviewapp/MainActivity.java
@@ -8,12 +8,19 @@ package za.cput.ict3.mycustomlistviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    MainActivity mA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
